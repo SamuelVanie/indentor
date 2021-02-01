77c04eec963ccebcd78e452f470c6ea9a443a2f3
samuel <damsektry@gmail.com>
2021-02-01 01:13:29 +0000

    Ajout d'un fichier pour les fonctions et creation de la premiere fonction putc

---
+++ fonctions/fonctions.c
@@ -0,0 +1,7 @@
+# include <stdio.h>
+# include "fonctions.h"
+
+void put_space(FILE *fp, int spaceNumber){
+    for(int i=0; i<=spaceNumber; i++)
+        fputc(" ", fp);
+}
---
+++ fonctions/fonctions.h
@@ -0,0 +1,6 @@
+#ifndef __FONCTIONS_H__
+#define __FONCTIONS_H__
+
+void put_space(FILE *fp, int spaceNumber);
+
+#endif
