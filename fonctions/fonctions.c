731ac79f195015fc763b03a7e67f612192ac7c7b
samuel <damsektry@gmail.com>
2021-02-01 16:12:24 +0000

    Boucle for reglee

--- fonctions/fonctions.c
+++ fonctions/fonctions.c
@@ -2,6 +2,6 @@
 # include "fonctions.h"
 
 void put_space(FILE *fp, int spaceNumber){
-    for(int i=0; i<=spaceNumber; i++)
-        fputc(" ", fp);
+    for(int i=0; i<spaceNumber; ++i)
+        fputc(' ', fp);
 }
--- main.c
+++ main.c
@@ -1,45 +1,58 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <stdbool.h>
-# include "stack-int/stack.h"
+# include "./fonctions/fonctions.h"
+# include "./stack-int/stack.h"
 
 int main(int argc, char **argv){
-    if(argc == 1){
-        FILE *fic1 = fopen(argv[1],"r");
-        FILE *fic2 = fopen("temp.txt","w");
-        Stack pile = new_stack();
-        pile = push_stack(pile, 0);
-        bool isInParenthesis = false;
-        while(!feof(fic1)){
-            char p = fgetc(fic1);
-            if(p == "("){
-                isInParenthesis = true;
-                fputc(p, fic2);
-            }else if(p == ")"){
-                isInParenthesis = false;
-                fputc(p, fic2);
-                if(fgetc(fic2) == " "){
-                    fputc("\n", fic2);
-                    fputs(" "*top_stack(pile), fic2);
+    FILE *fic1 = fopen("fichier.txt","r");
+    FILE *fic2 = fopen("temp.txt","w");
+    Stack pile = new_stack();
+    pile = push_stack(pile, 0);
+    bool isInParenthesis = false;
+    while(!feof(fic1)){
+        int p = getc(fic1);
+        if(p == '('){
+            isInParenthesis = true;
+            fputc(p, fic2);
+        }else if(p == ')'){
+            isInParenthesis = false;
+            fputc(p, fic2);
+            int s = getc(fic1);
+            if(s == ' '){
+                fputs("\n", fic2);
+                put_space(fic2, top_stack(pile));
+                pile = push_stack(pile, top_stack(pile)+4);
+            }else if(s == '{'){
+                fputc('{', fic2);
+                fputs("\n", fic2);
+                pile = push_stack(pile, top_stack(pile)+4);
+                put_space(fic2, top_stack(pile));
+            }
+        }else{
+            if(!isInParenthesis){
+                if(p == '{'){
+                    fputc(p, fic2);
+                    fputs("\n", fic2);
                     pile = push_stack(pile, top_stack(pile)+4);
-                }
-            }else{
-                if(!isInParenthesis){
-                    if(p == ";" || p == "{"){
-                        fputc(p, fic2);
-                        fputc("\n", fic2);
-                        fputs("    ", fic2);
-                        pile = push_stack(pile, top_stack(pile)+4);
-                    }else if(p == "}"){
-
-                    }else{
-                        fputc(p, fic2);
-                    }
-                    continue;
+                    put_space(fic2, top_stack(pile));
+                }else if(p == ';'){
+                    fputc(p, fic2);
+                    fputs("\n", fic2);
+                    put_space(fic2, top_stack(pile));
+                }else if(p == '}'){
+                    fputs("\n", fic2);
+                    pile = pop_stack(pile);
+                    put_space(fic2, top_stack(pile));
+                    fputc(p, fic2);
                 }else{
                     fputc(p, fic2);
                 }
+            }else{
+                fputc(p, fic2);
             }
         }
     }
+    fclose(fic2);
+    fclose(fic1);
 }
